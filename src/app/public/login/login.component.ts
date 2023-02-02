19fb15b4fdd31c3c7e8cd975b0ec3a9b51ed3351
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-02-02 22:10:49 +0530

    navigated

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -31,6 +31,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
         client_id: "939503022711-9m2d1ejrp8ufj52m52iab1klqeqdk37k.apps.googleusercontent.com",
         callback: ((res: any) => {
           console.log(res)
+          this.router.navigate(['/dashboard']);
         })
       });
       google.accounts.id.renderButton(
