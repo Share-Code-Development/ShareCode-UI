28bf4cce73a0e515e83f5c4da6f26d848168afb6
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-02-02 22:13:38 +0530

    google authg navigated

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { SocialAuthService } from "@abacritt/angularx-social-login";
 import { takeWhile } from 'rxjs';
 declare const google: any;
 
@@ -9,40 +9,22 @@ declare const google: any;
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, AfterViewInit {
+export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
-    // private authService: SocialAuthService
+    private authService: SocialAuthService
   ) { }
 
   ngOnInit(): void {
-    // this.authService.authState.pipe(takeWhile(() => true /* check if logged in to unsubscribe */)).subscribe((user) => {
-    //   console.log(user)
-    // });
+    this.authService.authState.pipe(takeWhile(() => true /* check if logged in to unsubscribe */)).subscribe((user) => {
+      console.log(user)
+      this.router.navigate(['/dashboard']);
+    });
   }
 
-  ngAfterViewInit(): void {
-    // window.handleCredentialResponse = (response: any) => {
-    //   console.log("Encoded JWT ID token: " + response.credential);
-    // }
-    // window.onload = function () {
-      google.accounts.id.initialize({
-        client_id: "939503022711-9m2d1ejrp8ufj52m52iab1klqeqdk37k.apps.googleusercontent.com",
-        callback: ((res: any) => {
-          console.log(res)
-          this.router.navigate(['/dashboard']);
-        })
-      });
-      google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }  // customization attributes
-      );
-      google.accounts.id.prompt(); // also display the One Tap dialog
-    // }
-  }
 
-  public onLigin() {
+  public onLogin() {
     this.router.navigate(['/dashboard']);
   }
 
