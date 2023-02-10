638ec603f6e55d6aa9110feda15732b89f8a3a40
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-02-10 21:53:11 +0530

    typos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
