8880769718f2789ef0fc9f480c49012fcc5959fb
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-02-12 01:28:21 +0530

    buildiong auth flow

---
+++ src/app/models/common.enum.ts
@@ -0,0 +1,5 @@
+export enum ELocalStorage {
+    currentUser = 'currentUser',
+    token = 'userToken'
+
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ELocalStorage } from '../models/common.enum';
 import { IUser } from '../models/user.interface';
 import { HttpService } from './http.service';
 
@@ -9,6 +10,7 @@ import { HttpService } from './http.service';
 export class UserService {
 
   public authState$: BehaviorSubject<IUser | null> = new BehaviorSubject<IUser | null>(null);
+  public isLoggedIn = false;
   private authEndpoint = 'auth';
   private userEndPoint = 'user';
 
@@ -16,8 +18,20 @@ export class UserService {
     private http: HttpService
   ) { }
 
-  public setupAuthState(user: IUser) {
+  public setupAuthState(user: IUser, token?: string) {
     this.authState$.next(user);
+    this.isLoggedIn = true;
+    localStorage.setItem(ELocalStorage.currentUser, JSON.stringify(user));
+    if (token) {
+      localStorage.setItem(ELocalStorage.token, token);
+    }
+  }
+
+  public logout() {
+    this.authState$.next(null);
+    this.isLoggedIn = false;
+    localStorage.removeItem(ELocalStorage.currentUser);
+    localStorage.removeItem(ELocalStorage.token);
   }
 
 
@@ -32,5 +46,10 @@ export class UserService {
   public googleLoginAsync(body: any) {
     return this.http.postAsync(body, `${this.authEndpoint}/google`)
   }
+
+  public forgotPasswordAsync(body: any) {
+    return this.http.postAsync(body, `${this.authEndpoint}/forgot-password`)
+  }
+
 
 }
