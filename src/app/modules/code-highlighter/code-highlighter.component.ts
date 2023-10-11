66ecd4b0657a05d377c2188d8a9853f4cc6a66ac
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-10-12 03:15:02 +0530

    cleanup post mgrate

--- src/app/modules/code-highlighter/code-highlighter.component.ts
+++ src/app/modules/code-highlighter/code-highlighter.component.ts
@@ -1,10 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { BehaviorSubject } from 'rxjs';
-import hljs from 'highlight.js';
-import { CodeJarContainer } from 'ngx-codejar';
-// import guessProgrammingLanguage from 'guess-programming-language'
-import { EditorComponent } from 'ngx-monaco-editor-v2';
 // @ts-ignore - no types available
 import langDetector from 'lang-detector';
 
@@ -22,19 +17,27 @@ declare const monaco: typeof import('monaco-editor');
     }
   ]
 })
-export class CodeHighlighterComponent implements ControlValueAccessor, AfterViewInit, OnDestroy {
-  @ViewChild('editorComponent') editorComponent!: EditorComponent;
+export class CodeHighlighterComponent implements ControlValueAccessor {
 
   @Input() public code: string = '';
 
-  @Input() language: string = 'javascript';
-  public readOnlyMode$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  protected editorOptions: any = { theme: 'vs-dark', automaticLayout: true };
+  public language: string = 'text';
+  @Input('language') set setLanguage(val: string) {
+    if (this.editor && val) {
+      monaco.editor.setModelLanguage(this.editor.getModel(), val.toLowerCase());
+    }
+  }
+
+  protected editorOptions: any = {
+    theme: 'vs-dark',
+    automaticLayout: true,
+    scrollBeyondLastLine: false,
+    language: this.language
+  };
+
   private editor: any;
 
   constructor(
-    private renderer: Renderer2,
-    private element: ElementRef
   ) {
   }
 
@@ -47,55 +50,20 @@ export class CodeHighlighterComponent implements ControlValueAccessor, AfterView
     let language = langDetector(this.code);
     console.log(language)
     if (language) {
-      this.language = language;
-      if (this.editor && language && language.toLowerCase() !== 'unknown') {
-        language = language.replace('C#', 'csharp');
-        language = language.replace('C++', 'cpp');
-        monaco.editor.setModelLanguage(this.editor.getModel(), language.toLowerCase());
+      language = language.toLowerCase()
+      if (this.editor && language && language !== 'unknown') {
+        this.language = language;
+        language = language.replace('c#', 'csharp');
+        language = language.replace('c++', 'cpp');
+        monaco.editor.setModelLanguage(this.editor.getModel(), language);
       }
     }
-    // guessProgrammingLanguage(this.code).then((language: string) => {
-    //   if (language) {
-    //     this.language = language;
-    //     if (this.editor) {
-    //       monaco.editor.setModelLanguage(this.editor.getModel(), language);
-    //       console.log(language)
-    //     }
-    //   }
-    // })
   }
 
   onEditorInit(editor: any) {
     this.editor = editor;
     this.guessCode()
   }
-
-  ngAfterViewInit(): void {
-    // this.readOnlyMode$.subscribe(val => {
-    //   if (val) {
-    //     this.renderer.setAttribute(this.element.nativeElement?.querySelector('.editor.hljs.ngx-codejar-editor'), 'contenteditable', 'false');
-    //   } else {
-    //     this.renderer.setAttribute(this.element.nativeElement?.querySelector('.editor.hljs.ngx-codejar-editor'), 'contenteditable', 'plaintext-only');
-    //   }
-    // })
-  }
-
-  ngOnDestroy(): void {
-    this.readOnlyMode$.unsubscribe();
-  }
-
-  // public highlightMethod = (editor: CodeJarContainer) => {
-  //   if (editor.textContent !== null && editor.textContent !== undefined) {
-  //     editor.innerHTML = hljs.highlight(editor.textContent, {
-  //       language: this.language
-  //     }).value;
-  //     guessProgrammingLanguage(editor.textContent).then((language: string) => {
-  //       if (language) {
-  //         this.language = language;
-  //       }
-  //     })
-  //   }
-  // }
 
   writeValue(obj: string): void {
     this.code = obj;
@@ -109,9 +77,6 @@ export class CodeHighlighterComponent implements ControlValueAccessor, AfterView
     this.onChange = fn;
   }
 
-  setDisabledState(isDisabled: boolean): void {
-    this.readOnlyMode$.next(isDisabled);
-  }
 
   private onChange: any = () => {
   };
--- src/app/modules/code-highlighter/code-highlighter.module.ts
+++ src/app/modules/code-highlighter/code-highlighter.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeHighlighterComponent } from './code-highlighter.component';
 import { SharedModule } from 'primeng/api';
-import { NgxCodejarModule } from 'ngx-codejar';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { FormsModule } from '@angular/forms';
 
@@ -13,7 +12,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     SharedModule,
-    NgxCodejarModule,
     MonacoEditorModule,
     FormsModule
   ],
